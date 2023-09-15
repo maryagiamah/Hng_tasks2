b966194e0aff1339ff1fe07bb0366ab2ed80e43b
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-09-15 21:20:56 +0100

    Create Delete_script.py

---
+++ tests/Delete_script.py
@@ -0,0 +1,10 @@
+import requests
+
+person_id = 1  # Replace with an existing person ID
+
+url = f'https://your-api-url.com/api/{person_id}'
+
+response = requests.delete(url)
+
+print(response.status_code)
+print(response.json())
